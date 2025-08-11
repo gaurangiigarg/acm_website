5f122fd4627007acb8fec8f62072e050974ae5d7
upesnavneet <kumarnavneet7765@gmail.com>
2025-08-12 02:34:15 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), glsl()], 
-  base: "/acm_website/",
+  base: '/acm_website/',
 })
