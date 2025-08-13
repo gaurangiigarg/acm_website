831758d8ae32f7b753076fc3e17f018d155da5a6
upesnavneet <kumarnavneet7765@gmail.com>
2025-08-14 02:30:43 +0530

    changed config file

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 import glsl from 'vite-plugin-glsl'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [tailwindcss(), react(), glsl()],
-    base: mode === 'production'
-      ? '/acm_website/' // for GitHub Pages
-      : '/',            // for local dev or Netlify
-  }
+export default defineConfig({
+  plugins: [tailwindcss(), react(), glsl()],
+  base: '/', // ✅ must be '/' for Netlify
 })
