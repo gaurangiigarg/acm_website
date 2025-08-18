b78ff862a176bcc478bf455e06f170a461f61e91
upesnavneet <kumarnavneet7765@gmail.com>
2025-08-19 02:35:05 +0530

    changed image

--- src/pages/About/Sponsors.jsx
+++ src/pages/About/Sponsors.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import './Sponsors.css'; // Import the stylesheet
+import TiltedCard from '../../../TitledCard/TiltedCard/TiltedCard'
 
-import Dell from '../../assets/img/logos/Dell.png';
 import IBM from '../../assets/img/logos/IBM.png'; // ✅ use IBM image
 
 const Sponsors = () => {
@@ -12,37 +12,67 @@ const Sponsors = () => {
         Since 2010 we've been working with <span className="highlight">amazing partners</span> to create meaningful impact and compelling experiences.
       </h2>
 
-      <div className="logo-grid">
+      <div className="sponsors-cards">
+    <TiltedCard
+      imageSrc="https://raw.githubusercontent.com/upesnavneet/acm_assets/main/img/logos/Dell.jpg"
+      altText="Dell Logo"
+      containerHeight="250px"
+      containerWidth="250px"
+      imageHeight="250px"
+      imageWidth="250px"
+      rotateAmplitude={12}
+      scaleOnHover={1}
+      showMobileWarning={false}
+      showTooltip={true}
+      displayOverlayContent={true}
+    />
 
-        {/* Dell Logo */}
-        <div className="logo-card">
-          <img src={Dell} alt="Dell Logo" className="logo-img" />
-        </div>
+    <TiltedCard
+      imageSrc="https://raw.githubusercontent.com/upesnavneet/acm_assets/main/img/logos/IBM.png"
+      altText="IBM Logo"
+      containerHeight="250px"
+      containerWidth="250px"
+      imageHeight="250px"
+      imageWidth="250px"
+      rotateAmplitude={12}
+      scaleOnHover={1}
+      showMobileWarning={false}
+      showTooltip={true}
+      displayOverlayContent={true}
+    />
 
-        {/* IBM Logo */}
-        <div className="logo-card">
-          <img src={IBM} alt="IBM Logo" className="logo-img" />  {/* ✅ Same style as Dell */}
-        </div>
+    <TiltedCard
+      imageSrc="https://raw.githubusercontent.com/upesnavneet/acm_assets/main/img/logos/Dell.jpg"
+      altText="Dell Logo"
+      containerHeight="250px"
+      containerWidth="250px"
+      imageHeight="250px"
+      imageWidth="250px"
+      rotateAmplitude={12}
+      scaleOnHover={1}
+      showMobileWarning={false}
+      showTooltip={true}
+      displayOverlayContent={true}
+    />
 
-        {/* ICPC Logo */}
-        <div className="logo-card">
-          <svg viewBox="0 0 24 24">
-            <path d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16zm-1-11h2v6h-2zm0 8h2v2h-2z"/>
-          </svg>
-        </div>
+    <TiltedCard
+      imageSrc="https://raw.githubusercontent.com/upesnavneet/acm_assets/main/img/logos/Dell.jpg"
+      altText="Dell Logo"
+      containerHeight="250px"
+      containerWidth="250px"
+      imageHeight="250px"
+      imageWidth="250px"
+      rotateAmplitude={12}
+      scaleOnHover={1}
+      showMobileWarning={false}
+      showTooltip={true}
+      displayOverlayContent={true}
+    />
+  </div>
+  
 
-        {/* Microsoft Logo */}
-        <div className="logo-card">
-          <svg viewBox="0 0 21 21">
-            <path d="M1 1h9v9H1zM11 1h9v9h-9zM1 11h9v9H1zM11 11h9v9h-9z"/>
-          </svg>
-        </div>
 
-        {/* Pynet Labs Logo */}
-        <div className="logo-card">
-          <span className="text-logo">Pynet Labs</span>
-        </div>
-      </div>
+      
     </section>
   );
 };
