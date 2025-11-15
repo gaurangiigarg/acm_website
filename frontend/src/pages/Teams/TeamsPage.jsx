a834b6b3bf7bd591c6f720c40e48f54d0c38ff73
upesnavneet <kumarnavneet7765@gmail.com>
2025-11-16 03:00:39 +0530

    hmm

--- frontend/src/pages/Teams/TeamsPage.jsx
+++ frontend/src/pages/Teams/TeamsPage.jsx
@@ -9,56 +9,66 @@ import TeamGrid3 from './TeamGrid3';
 import BOExecutivesHero from './BOExecutivesHero'
 import ExecutivesHero from './ExecutivesHero';
 import OfficeBearersHero from './OfficeBearersHero'; 
-
+import ColorBends from './background';
 
 const TeamsPage = () => {
-  useLenis(); 
-  return (
-    <>
+  useLenis(); 
+  return (
+    // UPDATED: Replaced fragment with a wrapper div
+    <div className="teams-page-container">
+      
+      {/* UPDATED: Placed ColorBends in its own background container */}
+      <div className="color-bends-background">
+        <ColorBends
+  colors={["#0c08f7"]}
+  rotation={0}
+  speed={0.2}
+  scale={1}
+  frequency={1}
+  warpStrength={1}
+  mouseInfluence={1}
+  parallax={0.5}
+  noise={0.01}
+/>
+      </div>
 
+      {/* UPDATED: Added a wrapper for all foreground content */}
+      <div className="teams-page-content-wrapper">
+        <Navbar />
+        {/* --- Top Section --- */}
+        <section className="team-section-wrapper">
+          <div className="top-divider"></div>
+          <div className="content-grid">
+            <div className="left-column">
+              <div>
+                <button className="gallery-button">GALLERY</button>
+              </div>
+              <p className="description">
+                We're a team driven by conviction. Meet our people.
+              </p>
+              <div className="team-member-footer">
+                ACM Student Chapter
+              </div>
+            </div>
+            <div className="right-column">
+              <h1 className="main-heading">
+                OUR TEAM
+                <span className="heading-number">ACM</span>
+              </h1>
+            </div>
+          </div>
+          <div className="bottom-divider"></div>
+        </section>
 
-      <Navbar />
-      {/* --- Top Section --- */}
-      <section className="team-section-wrapper">
-        <div className="top-divider"></div>
-        <div className="content-grid">
-          <div className="left-column">
-            <div>
-              <button className="gallery-button">GALLERY</button>
-            </div>
-            <p className="description">
-              We're a team driven by conviction. Meet our people.
-            </p>
-            <div className="team-member-footer">
-              ACM Student Chapter
-            </div>
-          </div>
-          <div className="right-column">
-            <h1 className="main-heading">
-              OUR TEAM
-              <span className="heading-number">ACM</span>
-            </h1>
-          </div>
-        </div>
-        <div className="bottom-divider"></div>
-      </section>
-
-
-      <BOExecutivesHero />
-
-      <TeamGrid1 />
-
-      <ExecutivesHero />
-
-      <TeamGrid2 />
-
-      <OfficeBearersHero />
-
-      <TeamGrid3 />
-
-
-    </>
-  );
+        <BOExecutivesHero />
+        <TeamGrid1 />
+        <ExecutivesHero />
+        <TeamGrid2 />
+        <OfficeBearersHero />
+        <TeamGrid3 />
+      </div> {/* End of content wrapper */}
+    </div> // End of main container
+  );
 };
 
 export default TeamsPage;
