8449952556def9dc2b6a698a0a3d7be059570532
upesnavneet <kumarnavneet7765@gmail.com>
2025-12-02 23:51:00 +0530

    hmm

--- frontend/src/pages/Events_Gallery/LadyAda.jsx
+++ frontend/src/pages/Events_Gallery/LadyAda.jsx
@@ -5,6 +5,17 @@ import image1 from '../../../src/assets/events_gallery/LadyAda/1.jpg';
 import image2 from '../../../src/assets/events_gallery/LadyAda/2.jpg';
 import image3 from '../../../src/assets/events_gallery/LadyAda/3.jpg';
 import image4 from '../../../src/assets/events_gallery/LadyAda/4.jpg';
+import image5 from '../../../src/assets/events_gallery/LadyAda/5.jpg';
+import image6 from '../../../src/assets/events_gallery/LadyAda/6.jpg';
+import image7 from '../../../src/assets/events_gallery/LadyAda/7.jpg';
+import image8 from '../../../src/assets/events_gallery/LadyAda/8.jpg';
+import image9 from '../../../src/assets/events_gallery/LadyAda/9.jpg';
+import image10 from '../../../src/assets/events_gallery/LadyAda/10.jpeg';
+import image11 from '../../../src/assets/events_gallery/LadyAda/11.jpeg';
+import image12 from '../../../src/assets/events_gallery/LadyAda/12.jpeg';
+import image13 from '../../../src/assets/events_gallery/LadyAda/13.jpeg';
+import image14 from '../../../src/assets/events_gallery/LadyAda/14.jpg';
+import image15 from '../../../src/assets/events_gallery/LadyAda/15.jpg';
 
 const items = [
     {
@@ -30,6 +41,72 @@ const items = [
       img: image4,
       url: null,
       height: 570,
+    },
+    {
+      id: "5",
+      img: image5,
+      url: null,
+      height: 500,
+    },
+    {
+      id: "6",
+      img: image6,
+      url: null,
+      height: 650,
+    },
+    {
+      id: "7",
+      img: image7,
+      url: null,
+      height: 800,
+    },
+    {
+      id: "8",
+      img: image8,
+      url: null,
+      height: 500,
+    },
+    {
+      id: "9",
+      img: image9,
+      url: null,
+      height: 500,
+    },
+    {
+      id: "10",
+      img: image10,
+      url: null,
+      height: 700,
+    },
+    {
+      id: "11",
+      img: image11,
+      url: null,
+      height: 700,
+    },
+    {
+      id: "12",
+      img: image12,
+      url: null,
+      height: 650,
+    },
+    {
+      id: "13",
+      img: image13,
+      url: null,
+      height: 650,
+    },
+    {
+      id: "14",
+      img: image14,
+      url: null,
+      height: 425,
+    },
+    {
+      id: "15",
+      img: image15,
+      url: null,
+      height: 400,
     },
 
 ];
